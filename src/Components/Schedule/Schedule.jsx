1fdad755938f340f6b2268b8ffbc3147a0ed7d8f
Christen-Wesa <christenSh.wesa@yahoo.com>
2025-10-19 13:37:46 -0400

    first commit

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -17,7 +17,7 @@ export default function Schedule() {
         <h2 className='py-4'>Upcoming Events</h2>
         <div className="row">
           <div className="col-md-6 col-sm-10">
-          <iframe className='w-100' src="https://calendar.google.com/calendar/embed?height=800&wkst=1&ctz=America%2FNew_York&showPrint=0&showTitle=0&showNav=0&showCalendars=0&showTz=0&showTabs=0&mode=AGENDA&src=ZnIuc2FtdWVsdEBnbWFpbC5jb20&color=%23C0CA33" height={800} frameborder="0" scrolling="no"></iframe>          
+            <iframe className='w-100' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&mode=AGENDA&src=ZnIuc2FtdWVsdEBnbWFpbC5jb20&color=%23c0ca33" height={600} frameborder="0"></iframe>
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@ export default function Schedule() {
         <h2 className='text-center py-3'>Full Calender</h2>
         <div className="row">
           <div className="col-12">
-            <iframe className='mt-4 w-100' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&showTitle=0&showTz=0&src=ZnIuc2FtdWVsdEBnbWFpbC5jb20&color=%23C0CA33" height={600} frameborder="0" scrolling="no"></iframe>    
+            <iframe className='mt-4 w-100' src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&src=ZnIuc2FtdWVsdEBnbWFpbC5jb20&color=%23c0ca33" height={600} frameborder="0" scrolling="no"></iframe>
           </div>
         </div>
       </div>
